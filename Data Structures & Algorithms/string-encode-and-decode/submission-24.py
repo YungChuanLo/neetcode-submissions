43f3f090b5cce122d62e4a7f23abc4373faf87a7
YungChuan Lo <frank105311499@gmail.com>
2026-05-18 15:14:23 -0700

    Add: string-encode-and-decode - submission-24

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-24.py
@@ -0,0 +1,44 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        
+        encoded_list = []
+        encoded_str = ""
+        for s in strs:
+            if(s == ""):
+                encoded_str += "x "
+            else:
+            
+                encoded_list = [ord(char) for char in s]
+                for number in encoded_list:
+                    encoded_str += str(number) + ","
+                
+                encoded_str = encoded_str[:-1]
+                encoded_str += " "
+        
+
+        return encoded_str
+                
+
+    def decode(self, s: str) -> List[str]:
+        splited = s.split()
+        decoded_list = []
+        decoded_str = ""
+        for number in splited:
+            if (number == 'x'):
+                decoded_str = ""
+            else:
+                digits = number.split(',')
+                for digit in digits:
+                    print(digit)
+                    print(type(digit))
+                    decoded_str += chr(int(digit))
+
+
+            decoded_list.append(decoded_str)
+            decoded_str = ""
+
+        return decoded_list
+            
+
+            
